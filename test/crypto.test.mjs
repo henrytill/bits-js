e86d36b9f848024e3c6f7959e4b857e94d24d767
Henry Till <henrytill@gmail.com>
2023-03-24 11:44:14 -0700

    switch to web-test-runner

--- test/crypto.test.mjs
+++ test/crypto.test.mjs
@@ -1,6 +1,12 @@
 // @ts-check
 
-import { makePassword, makePlaintext, encrypt, decrypt } from './crypto.mjs';
+import { expect } from '@esm-bundle/chai';
+import {
+  makePassword,
+  makePlaintext,
+  encrypt,
+  decrypt,
+} from '../static/crypto.mjs';
 
 if (window.isSecureContext) {
   console.log('This is a secure context.');
@@ -8,38 +14,32 @@ if (window.isSecureContext) {
   console.warn('This is an insecure context.');
 }
 
-mocha.setup('bdd');
-mocha.checkLeaks();
-
 const SHOULD_CONSTRUCT =
   'should construct an object whose underlying text is the same as the text that was passed as an argument';
 
-describe('makePassword()', function () {
-  it(SHOULD_CONSTRUCT, function () {
+describe('makePassword()', () => {
+  it(SHOULD_CONSTRUCT, () => {
     const text = 'A moving stream of information';
     const password = makePassword(text);
-    chai.assert.equal(text, password.text());
+    expect(text).to.equal(password.text());
   });
 });
 
-describe('makePlaintext()', function () {
-  it(SHOULD_CONSTRUCT, function () {
+describe('makePlaintext()', () => {
+  it(SHOULD_CONSTRUCT, () => {
     const text = 'A moving stream of information';
     const plaintext = makePlaintext(text);
-    chai.assert.equal(text, plaintext.text());
+    expect(text).to.equal(plaintext.text());
   });
 });
 
-describe('encrypt()', function () {
-  it('should round-trip', async function () {
+describe('encrypt()', () => {
+  it('should round-trip', async () => {
     const message = 'A moving stream of information';
     const expected = makePlaintext(message);
     const password = makePassword('abc123');
     const { ciphertext, salt, iv } = await encrypt(password, expected);
     const actual = await decrypt(password, ciphertext, salt, iv);
-    chai.assert.equal(expected.text(), actual.text());
+    expect(expected.text()).to.equal(actual.text());
   });
 });
-
-console.log('Running Mocha tests...');
-mocha.run();
