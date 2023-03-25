4b41f88d74ad5583639a2c7025b46986476c9d8a
Henry Till <henrytill@gmail.com>
2023-03-25 15:30:07 +0000

    use playwright

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,4 +1,12 @@
+import { playwrightLauncher } from '@web/test-runner-playwright';
+
 export default {
   nodeResolve: true,
   files: ['test/**/*.test.mjs'],
+  playwright: true,
+  browsers: [
+    playwrightLauncher({ product: 'firefox' }),
+    playwrightLauncher({ product: 'chromium' }),
+    playwrightLauncher({ product: 'webkit' }),
+  ],
 };
