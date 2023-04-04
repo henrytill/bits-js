c02ebd8407ac5d205007128ed2cb1e06d15bf939
Henry Till <henrytill@gmail.com>
2023-04-04 18:09:58 +0000

    crypto: use namespace import

--- test/crypto.test.mjs
+++ test/crypto.test.mjs
@@ -1,15 +1,7 @@
 // @ts-check
 
 import { expect } from '@esm-bundle/chai';
-import {
-  makeKey,
-  makePassword,
-  makePlaintext,
-  makeSalt,
-  makeUUIDGenerator,
-  encrypt,
-  decrypt,
-} from '../static/crypto.mjs';
+import * as crypto from '../static/crypto.mjs';
 
 const SHOULD_CONSTRUCT =
   'should construct an object whose underlying text is the same as the text that was passed as an argument';
@@ -18,14 +10,14 @@ const text = 'A moving stream of information';
 
 describe('makePassword()', function () {
   it(SHOULD_CONSTRUCT, function () {
-    const password = makePassword(text);
+    const password = crypto.makePassword(text);
     expect(text).to.equal(password.text());
   });
 
   it('should deterministically create keys', async function () {
-    const a = makePassword('abc123');
-    const b = makePassword('abc123');
-    const salt = makeSalt();
+    const a = crypto.makePassword('abc123');
+    const b = crypto.makePassword('abc123');
+    const salt = crypto.makeSalt();
     const aKey = await a.generateKey(salt);
     const bKey = await b.generateKey(salt);
     expect(aKey).to.deep.equal(bKey);
@@ -34,26 +26,29 @@ describe('makePassword()', function () {
 
 describe('makePlaintext()', function () {
   it(SHOULD_CONSTRUCT, function () {
-    const plaintext = makePlaintext(text);
+    const plaintext = crypto.makePlaintext(text);
     expect(text).to.equal(plaintext.text());
   });
 });
 
 describe('encrypt()', function () {
   it('should round-trip', async function () {
-    const expected = makePlaintext(text);
-    const password = makePassword('abc123');
-    const { ciphertext, salt, iv } = await encrypt(password, expected);
-    const actual = await decrypt(password, ciphertext, salt, iv);
+    const expected = crypto.makePlaintext(text);
+    const password = crypto.makePassword('abc123');
+    const { ciphertext, salt, iv } = await crypto.encrypt(password, expected);
+    const actual = await crypto.decrypt(password, ciphertext, salt, iv);
     expect(expected.text()).to.equal(actual.text());
   });
 
   it('should decrypt with a duplicate password', async function () {
-    const expected = makePlaintext(text);
-    const passwordEncrypt = makePassword('abc123');
-    const { ciphertext, salt, iv } = await encrypt(passwordEncrypt, expected);
-    const passwordDecrypt = makePassword('abc123');
-    const actual = await decrypt(passwordDecrypt, ciphertext, salt, iv);
+    const expected = crypto.makePlaintext(text);
+    const passwordEncrypt = crypto.makePassword('abc123');
+    const { ciphertext, salt, iv } = await crypto.encrypt(
+      passwordEncrypt,
+      expected,
+    );
+    const passwordDecrypt = crypto.makePassword('abc123');
+    const actual = await crypto.decrypt(passwordDecrypt, ciphertext, salt, iv);
     expect(expected.text()).to.equal(actual.text());
   });
 });
@@ -69,7 +64,7 @@ describe('makeKey()', function () {
   let key;
 
   it('should generate a Promise', function () {
-    promise = makeKey(storageKey);
+    promise = crypto.makeKey(storageKey);
     expect(promise).to.be.an.instanceOf(Promise);
   });
 
@@ -79,7 +74,7 @@ describe('makeKey()', function () {
   });
 
   it('should fetch an existing key from localStorage', async function () {
-    const otherKey = await makeKey(storageKey);
+    const otherKey = await crypto.makeKey(storageKey);
     expect(key).to.deep.equal(otherKey);
   });
 
@@ -93,7 +88,7 @@ describe('makeUUIDGenerator()', function () {
   let generator;
 
   it('should generate a UUIDGenerator', function () {
-    generator = makeUUIDGenerator();
+    generator = crypto.makeUUIDGenerator();
     expect(generator).to.be.an.instanceOf(Object);
     expect(generator.generate).to.be.an.instanceOf(Function);
   });
@@ -108,7 +103,7 @@ describe('UUIDGenerator', function () {
     let uuid;
 
     before(function () {
-      generator = makeUUIDGenerator();
+      generator = crypto.makeUUIDGenerator();
     });
 
     it('should generate a UUID', function () {
@@ -140,7 +135,7 @@ describe('UUIDGenerator', function () {
       const uuids = new Set();
       for (let i = 0; i < 1000; i++) {
         uuids.add(generator.generate().get());
-        uuids.add(makeUUIDGenerator().generate().get());
+        uuids.add(crypto.makeUUIDGenerator().generate().get());
       }
       expect(uuids.size).to.equal(2000);
     });
