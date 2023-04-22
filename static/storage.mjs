b0e7dea389ab0a708cd5b454f9a6babcb476fa98
Henry Till <henrytill@gmail.com>
2023-04-22 16:39:47 +0000

    storage: clean up

--- static/storage.mjs
+++ static/storage.mjs
@@ -1,6 +1,18 @@
 /**
- * @typedef {{ getItem: (key: string) => string | null }} HasGetItem
- * @typedef {{ setItem: (key: string, value: string) => void }} HasSetItem
+ * An object that has a `getItem` method.
+ *
+ * @typedef {Object} HasGetItem
+ * @property {(key: string) => string | null} getItem
+ */
+
+/**
+ * An object that has a `setItem` method.
+ *
+ * @typedef {Object} HasSetItem
+ * @property {(key: string, value: string) => void} setItem
+ */
+
+/**
  * @typedef {HasGetItem & HasSetItem} HasStorage
  */
 
