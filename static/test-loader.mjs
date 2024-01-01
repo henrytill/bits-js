a7553cd6ec03c60ecb96cebcfcba2634ec1ae47a
Henry Till <henrytill@gmail.com>
2024-01-01 14:30:35 -0800

    remove {crypto,db,storage}.mjs and related

--- static/test-loader.mjs
+++ static/test-loader.mjs
@@ -1,26 +1,12 @@
 /** @type {any} */
 let testModule;
 /** @type {any} */
-let cryptoTestModule;
-/** @type {any} */
-let dbTestModule;
-/** @type {any} */
 let pinboardTestModule;
 
 /**
  * @returns {void}
  */
 function clearResults() {
-    const cryptoResultsDiv = document.getElementById('cryptoResults');
-    if (!cryptoResultsDiv) {
-        throw new Error('cryptoResultsDiv not found');
-    }
-    cryptoResultsDiv.innerHTML = '';
-    const dbResultsDiv = document.getElementById('dbResults');
-    if (!dbResultsDiv) {
-        throw new Error('dbResultsDiv not found');
-    }
-    dbResultsDiv.innerHTML = '';
     const pinboardResultsDiv = document.getElementById('pinboardResults');
     if (!pinboardResultsDiv) {
         throw new Error('pinboardResultsDiv not found');
@@ -43,8 +29,6 @@ const makePath = (stamp, timestamp, path) => (stamp ? `${path}?v=${timestamp}` :
 async function loadModules(stamp = true) {
     const timestamp = new Date().getTime();
     testModule = await import(makePath(stamp, timestamp, './test.mjs'));
-    cryptoTestModule = await import(makePath(stamp, timestamp, './crypto.test.mjs'));
-    dbTestModule = await import(makePath(stamp, timestamp, './db.test.mjs'));
     pinboardTestModule = await import(makePath(stamp, timestamp, './pinboard.test.mjs'));
 }
 
@@ -53,8 +37,6 @@ async function loadModules(stamp = true) {
  */
 async function runTests() {
     await Promise.all([
-        testModule.runner(cryptoTestModule.tests, document.getElementById('cryptoResults')),
-        testModule.runner(dbTestModule.tests, document.getElementById('dbResults')),
         testModule.runner(pinboardTestModule.tests, document.getElementById('pinboardResults')),
     ]);
 }
