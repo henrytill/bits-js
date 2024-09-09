3ee2987f7c237e0eee343120f763d3d0c7f8345c
Henry Till <henrytill@gmail.com>
2024-09-08 19:35:11 -0700

    pinboard.{,test.}mjs: update, add parseHtml

--- static/pinboard.mjs
+++ static/pinboard.mjs
@@ -2,10 +2,10 @@
  * @typedef {Object} Post
  * @property {URL} href
  * @property {Date} time
- * @property {string?} description
- * @property {string?} extended
+ * @property {string?} [description]
+ * @property {string?} [extended]
  * @property {string[]} tag
- * @property {string} hash
+ * @property {string?} [hash]
  * @property {boolean} shared
  * @property {boolean} toread
  */
@@ -15,9 +15,11 @@
  * @param {string} xml
  * @returns {Post[]}
  */
-export const parsePosts = (parser, xml) => {
-  const document = parser.parseFromString(xml, 'application/xml');
+export const parseXml = (parser, xml) => {
   const ret = [];
+
+  const document = parser.parseFromString(xml, 'application/xml');
+
   for (let post of document.querySelectorAll('post')) {
     const hrefstr = post.getAttribute('href');
     if (hrefstr === null) {
@@ -31,8 +33,8 @@ export const parsePosts = (parser, xml) => {
       continue;
     }
     const time = new Date(timestr);
-    const description = post.getAttribute('description');
-    const extended = post.getAttribute('extended');
+    const description = post.getAttribute('description') || undefined;
+    const extended = post.getAttribute('extended') || undefined;
     const tag = post.getAttribute('tag')?.split(' ') ?? [];
     const hash = post.getAttribute('hash');
     if (hash === null) {
@@ -41,7 +43,78 @@ export const parsePosts = (parser, xml) => {
     }
     const shared = (post.getAttribute('shared') ?? 'no') === 'yes';
     const toread = (post.getAttribute('toread') ?? 'no') === 'yes';
-    ret.push({ href, time, description, extended, tag, hash, shared, toread });
+    ret.push({
+      href,
+      time,
+      tag,
+      hash,
+      shared,
+      toread,
+      ...(description !== undefined && { description }),
+      ...(extended !== undefined && { extended }),
+    });
   }
+
+  return ret;
+};
+
+/**
+ * @param {DOMParser} parser
+ * @param {string} html
+ * @returns {Post[]}
+ */
+export const parseHtml = (parser, html) => {
+  const ret = [];
+
+  /** @type {NodeListOf<HTMLAnchorElement> | HTMLAnchorElement | undefined} */
+  let anchor_selector;
+  /** @type {string | undefined} */
+  let description;
+  /** @type {string | undefined} */
+  let extended;
+
+  const document = parser.parseFromString(html, 'text/html');
+
+  for (let dt_selector of document.querySelectorAll('dt')) {
+    description = undefined;
+    extended = undefined;
+
+    anchor_selector = dt_selector.querySelectorAll('a');
+    if (anchor_selector.length !== 1) {
+      console.error(`anchor_selector.length = ${anchor_selector.length}`);
+      continue;
+    }
+    const [anchor_element] = anchor_selector;
+    const hrefstr = anchor_element.getAttribute('href');
+    if (hrefstr === null) {
+      console.error('Attempted to parse post without href');
+      continue;
+    }
+    const href = new URL(hrefstr);
+    const timestr = anchor_element.getAttribute('add_date');
+    if (timestr === null) {
+      console.error('Attempted to parse post without time');
+      continue;
+    }
+    const time = new Date(parseInt(timestr) * 1000);
+    const tag = anchor_element.getAttribute('tags')?.split(',') ?? [];
+    const shared = (anchor_element.getAttribute('private') ?? '1') === '0';
+    const toread = (anchor_element.getAttribute('toread') ?? '0') === '1';
+    description = anchor_element.textContent?.trim() ?? undefined;
+    const dd_element = dt_selector.nextElementSibling;
+    if (dd_element && dd_element.tagName.toLowerCase() === 'dd') {
+      extended = dd_element.textContent?.trim() ?? undefined;
+    }
+    ret.push({
+      href,
+      time,
+      tag,
+      shared,
+      toread,
+      ...(description !== undefined && { description }),
+      ...(extended !== undefined && { extended }),
+    });
+  }
+
   return ret;
 };
--- static/pinboard.test.mjs
+++ static/pinboard.test.mjs
@@ -3,17 +3,37 @@ import * as test from './test.mjs';
 
 export const PINBOARD_XML = `<?xml version="1.0" encoding="UTF-8"?>
 <posts user="asmithee">
-<post href="http://c-faq.com/decl/spiral.anderson.html" time="2022-05-21T06:26:01Z" description="Clockwise/Spiral Rule" extended="" tag="c c++" hash="970c1c894ea099f677fbfa18e74e5e31"  shared="no"  />
-<post href="https://www.intel.com/content/www/us/en/developer/tools/oneapi/vtune-profiler.html#gs.x8oazh" time="2022-04-13T13:12:10Z" description="Fix Performance Bottlenecks with Intel® VTune™ Profiler" extended="" tag="performance profiling tools" hash="2ab1611711c8bb5ed9273b8f4b612fca"  shared="no"  />
+<post href="http://c-faq.com/decl/spiral.anderson.html" time="2022-05-21T06:26:01Z" description="Clockwise/Spiral Rule" extended="" tag="c c++" hash="970c1c894ea099f677fbfa18e74e5e31"  shared="no" toread="yes" />
+<post href="https://www.intel.com/content/www/us/en/developer/tools/oneapi/vtune-profiler.html#gs.x8oazh" time="2022-04-13T13:12:10Z" description="Fix Performance Bottlenecks with Intel® VTune™ Profiler" extended="" tag="performance profiling tools" hash="2ab1611711c8bb5ed9273b8f4b612fca"  shared="yes"  />
 <post href="https://docs.microsoft.com/en-us/sysinternals/downloads/procmon" time="2020-11-24T02:24:59Z" description="Process Monitor - Windows Sysinternals | Microsoft Docs" extended="Monitor file system, Registry, process, thread and DLL activity in real-time." tag="windows-dev" hash="b87fcc08e549f4076ebeeeaf095482a5"  shared="no"  />
 </posts>
 `;
 
-const parseTest = test.makeTest('parsePosts should parse posts', () => {
+const parseXmlTest = test.makeTest('parseXml should parse posts', () => {
   const parser = new DOMParser();
-  const posts = pinboard.parsePosts(parser, PINBOARD_XML);
+  const posts = pinboard.parseXml(parser, PINBOARD_XML);
   console.log(posts);
   test.assert(posts.length === 3);
 });
 
-export const tests = [parseTest];
+export const PINBOARD_HTML = `<!DOCTYPE NETSCAPE-Bookmark-file-1>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+<TITLE>Pinboard Bookmarks</TITLE>
+<H1>Bookmarks</H1>
+<DL><p><DT><A HREF="http://c-faq.com/decl/spiral.anderson.html" ADD_DATE="1653114361" PRIVATE="0" TOREAD="0" TAGS="c,c++">Clockwise/Spiral Rule</A>
+
+<DT><A HREF="https://docs.microsoft.com/en-us/sysinternals/downloads/procmon" ADD_DATE="1606184699" PRIVATE="1" TOREAD="0" TAGS="windows-dev">Process Monitor - Windows Sysinternals | Microsoft Docs</A>
+<DD>Monitor file system, Registry, process, thread and DLL activity in real-time.
+
+<DT><A HREF="https://www.intel.com/content/www/us/en/developer/tools/oneapi/vtune-profiler.html" ADD_DATE="1649855530" PRIVATE="1" TOREAD="1" TAGS="performance,profiling,tools,toread">Fix Performance Bottlenecks with Intel® VTune™ Profiler</A>
+</DL></p>
+`;
+
+const parseHtmlTest = test.makeTest('parseHtml should parse posts', () => {
+  const parser = new DOMParser();
+  const posts = pinboard.parseHtml(parser, PINBOARD_HTML);
+  console.log(posts);
+  test.assert(posts.length === 3);
+});
+
+export const tests = [parseXmlTest, parseHtmlTest];
